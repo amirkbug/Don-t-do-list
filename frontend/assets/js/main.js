731bd38fb5c001a93e03bacd9df04c58e5755f06
amirkbug <amirworkplacett@gmail.com>
2025-08-20 12:39:27 +0330

    Buttons complete
    the variety of buttons complete in a neat way

---
+++ frontend/assets/js/main.js
@@ -0,0 +1,11 @@
+console.log("script is runing");
+
+const btn = document.querySelector(".btn--video--bg");
+const video = document.querySelector(".btn-video");
+
+btn.addEventListener("mouseenter", () => {
+  video.playbackRate = 3;
+});
+btn.addEventListener("mouseleave", () => {
+  video.playbackRate = 1;
+});
