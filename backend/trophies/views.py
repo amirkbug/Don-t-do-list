52dabc2ab2a97387bba25237882ac9f8a388a832
amirkbug <amirworkplacett@gmail.com>
2026-02-08 13:01:25 +0330

    we have paginator FINALLY!!
    i made paginator here and some changes to static to make it more understandable

--- backend/trophies/views.py
+++ backend/trophies/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render , get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Trophies
-
+from django.core.paginator import Paginator , PageNotAnInteger , EmptyPage
 
 def trophies(request):
+
     trophies = Trophies.objects.all()
+    trophies = Paginator(trophies , 6)
+    try:
+        page_num = request.GET.get("page")
+        trophies = trophies.get_page(page_num)
+    except PageNotAnInteger:
+        trophies = trophies.get_page(1)
+    except:
+        trophies = trophies.get_page(1)
     context = {
+        'page_num':page_num,
         'trophies' : trophies
     }
     return render(request ,"trophies/trophy.html",context=context)
