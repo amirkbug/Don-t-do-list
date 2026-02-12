c35b23ad21cdda672bb96b9b76757cd5cbadf9c6
amirkbug <amirworkplacett@gmail.com>
2026-02-12 19:31:04 +0330

    search and category buttons done
    i made search and category buttons in frontend

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1875,3 +1875,24 @@ function passwordReset() {
   });
 }
 passwordReset();
+
+//fn for category btns
+function categoryButtons() {
+  const btns = document.querySelectorAll(".small--buttons--v2");
+
+  btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const isActive = btn.classList.contains("active");
+
+      //delete all active classes
+      btns.forEach((b) => b.classList.remove("active"));
+
+      //if there was no active add it
+      if (!isActive) {
+        btn.classList.add("active");
+      }
+    });
+  });
+}
+
+categoryButtons();
