b17ae5cf8133e4eaa81000ba3db57eef2b03024a
amirkbug <amirworkplacett@gmail.com>
2026-02-16 11:57:29 +0330

    trophy category btns , search completed

--- backend/trophies/urls.py
+++ backend/trophies/urls.py
@@ -4,5 +4,7 @@ app_name = "trophies"
 
 urlpatterns = [
     path("",trophies,name="trophies-page"),
+    path("trophy-category/<str:trophy_cat>", trophies ,name="trophy_category"),
+    path("trophy-search/<str:trophy_search_variable>", trophies ,name="trophy_search"),
     path('trohpy-details/<int:trophy_id>',trophy_details , name='trophy_details'),
 ]
--- backend/trophies/views.py
+++ backend/trophies/views.py
@@ -5,34 +5,38 @@ from .models import Trophies
 from django.contrib import messages
 from django.core.paginator import Paginator , PageNotAnInteger , EmptyPage
 
-def trophies(request):
+def trophies(request,**kwargs):
 
-    # get all trophies from the model
-    trophies = Trophies.objects.all()
+    
+    # query variables
+    trophy_category = kwargs.get("trophy_cat") 
+    trophy_search = request.GET.get("search") 
 
-    # category filter
-    trophy_type = request.GET.get("type")
-    if trophy_type:
-        trophies = trophies.filter(trophy_type=trophy_type)
-        if trophy_type != "DO" and trophy_type != "DONT":
-            trophies = Trophies.objects.all()
-            messages.add_message(request, messages.ERROR , f'The category "{trophy_type}" is invalid.')
-
-
-    # search filter
-    search_query = request.GET.get("search")
-    if search_query:
-        trophies = trophies.filter(name__contains=search_query)
+    # search and category query 
+    if trophy_search and trophy_category:
+        trophies = Trophies.objects.filter(name__icontains=trophy_search,trophy_type=trophy_category)
         if not trophies.exists():
             trophies = Trophies.objects.all()
-            messages.add_message(request, messages.ERROR , "There were no search results.")
-    
-    
-        
-    # then give it to paginator 
-    trophies = Paginator(trophies , 6)
-        
+            messages.add_message(request , messages.ERROR , 'There were no search results.')
+    # category query 
+    elif trophy_category:
+        trophies = Trophies.objects.filter(trophy_type=trophy_category)
+        if not trophies.exists():
+            trophies = Trophies.objects.all()
+            messages.add_message(request , messages.ERROR , f'The category "{trophy_category}" is invalid.')
+    # search query 
+    elif trophy_search:
+        trophies = Trophies.objects.filter(name__icontains=trophy_search)
+        if not trophies.exists():
+            trophies = Trophies.objects.all()
+            messages.add_message(request , messages.ERROR , 'There were no search results.')
+    else:
+        trophies = Trophies.objects.all()
+   
+
     # paginator stuff
+    trophies = Paginator(trophies , 6)
+    
     try:
         page_num = request.GET.get("page")
         trophies = trophies.get_page(page_num)
@@ -40,11 +44,12 @@ def trophies(request):
         trophies = trophies.get_page(1)
     except:
         trophies = trophies.get_page(1)
-    
-    # put it in the page
     context = {
         'page_num':page_num,
-        'trophies' : trophies
+        'trophies' : trophies,
+        # we send these in for having them in the url all the time
+        'trophy_category' : trophy_category,
+        'trophy_search': trophy_search
     }
     return render(request ,"trophies/trophy.html",context=context)
 
